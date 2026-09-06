e5517ba63452ca61c8d5fe4ff483def70c28ebb9
Xjaxon <1265433359@qq.com>
2026-09-06 09:57:10 +0800

    chore: export transaction oracle factory

--- src/chaosatlas/oracles/__init__.py
+++ src/chaosatlas/oracles/__init__.py
@@ -9,6 +9,7 @@ from chaosatlas.oracles.registry import (
     build_default_oracle_registry,
 )
 from chaosatlas.oracles.replay import TransactionReplayer, TransactionWorkflowOracle, UrllibHttpTransport
+from chaosatlas.oracles.transaction_factory import TransactionOracleFactory
 from chaosatlas.oracles.transaction_contracts import freeze_approved_contract, validate_transaction_contract
 
 __all__ = [
@@ -21,6 +22,7 @@ __all__ = [
     "TransactionReplayer",
     "TransactionWorkflowOracle",
     "UrllibHttpTransport",
+    "TransactionOracleFactory",
     "WorkflowOracle",
     "build_default_oracle_registry",
     "freeze_approved_contract",
